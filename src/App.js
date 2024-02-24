fb6434c5b86db9479d4115d7e8837183fea0cffc
kathrynchoii <kathrynchoi1064@gmail.com>
2024-02-24 16:12:10 -0500

    Added BMI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/homepage';
 import Navbar from './components/navbar';
+import BMI from './components/BMI';
+import BMR from './components/bmr'; // Check this import statement
+import About from './components/About'; // Check the case of the component name here
 
 function App() {
+    useEffect(() => {
+        document.title = 'BMR APP';
+    }, []);
+
     return (
-        <div className = "App">
-        <Navbar />
-        <Homepage />
-        </div>
+        <Router> 
+            <div className="App">
+                <Navbar />
+                <Routes>
+                    <Route path="/homepage" element={<Homepage />} />
+                    <Route path="/BMR" element={<BMR />} /> {/* Ensure that the path matches */}
+                    <Route path="/BMI" element={<BMI />} />
+                    <Route path="/about" element={<About />} /> {/* Check the case of the component name here */}
+                </Routes>
+            </div>
+        </Router>
     );
-}   
+}
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">BMR Calculator</a></li>
-                <li><a href="/about">About</a></li>
-                {/* Add more navigation items as needed */}
-            </ul>
+            <div className = "title">
+                    <span className = "title-stuff">Wellness Watcher</span>
+            </div>
+
+            <div className = "container"> 
+                <ul className = "navbar-nav">
+                    <li className="nav-item"><Link to="/homepage" className="nav-link">Home</Link></li>
+                    <li className="nav-item"><Link to="/BMR" className="nav-link">BMR</Link></li>
+                    <li className="nav-item"><Link to="/BMI" className="nav-link">BMI</Link></li>
+                    <li className="nav-item"><Link to="/About" className="nav-link">About</Link></li>
+                </ul>
+            </div>
         </nav>
     );
 };
